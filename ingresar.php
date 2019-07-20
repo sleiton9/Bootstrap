a7c4848448a5b581d062236d45ee709dc9d9534d
Sebastian Leiton <sebas_garzon@hotmail.com>
2019-07-20 13:02:35 -0500

    agregando base de datos

---
+++ ingresar.php
@@ -0,0 +1,19 @@
+<?php
+$data = '{"concepto": "Curso de PHP", "subtotal":"200", "ID":"1"}';
+$url = "https://pedas-sistemas01.firebaseio.com/presupuestos.json";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/plain'));
+
+$response = curl_exec($ch);
+if(curl_errno($ch)){
+  echo 'Error:' . curl_errno($ch);
+}else {
+  echo "Ya insertó";
+}
+
+?>
